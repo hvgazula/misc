0fe2553b9f1d1aee2a8ba75e57c81a2a84a9f18e
hvgazula <hvgazula@users.noreply.github.com>
2022-02-01 11:21:04 -0500

    corrected for loop by jei

--- optimized_zoom.py
+++ optimized_zoom.py
@@ -67,7 +67,7 @@ def def_scipy_zoom(small_bias, factors):
 
 
 @timer_func
-def bias_jei_zoom(small_bias, factors):
+def bias_jei_zoom(small_bias, factors, grid_mode=True):
     # return bias_field
     Sx, Sy, Sz = small_bias.shape
     Bx, By, Bz = np.multiply(small_bias.shape, factors)
@@ -75,8 +75,13 @@ def bias_jei_zoom(small_bias, factors):
     I = small_bias
     A = np.zeros((Bx, Sy, Sz))
     Xs = np.arange(Bx)
-    Xs_prime = Xs / factors[0]
-    f = np.floor(Xs_prime).astype(np.int)
+    # Xs_prime = Xs / factors[0]
+    if grid_mode:
+        Xs_prime = (1 - factors[0])/(2 * factors[0]) + Xs/factors[0] 
+    else:
+        Xs_prime = Xs * ((Sx-1) / (Bx-1))
+
+    f = np.maximum(0, np.floor(Xs_prime).astype(np.int))
     c = np.minimum(f + 1, Sx - 1)
 
     # w1 = c - Xs_prime
@@ -90,8 +95,13 @@ def bias_jei_zoom(small_bias, factors):
     I = A
     A = np.zeros((Bx, By, Sz))
     Ys = np.arange(By)
-    Ys_prime = Ys / factors[1]
-    f = np.floor(Ys_prime).astype(np.int)
+    # Ys_prime = Ys / factors[1]
+    if grid_mode:
+        Ys_prime = (1 - factors[1])/(2 * factors[1]) + Ys/factors[1]
+    else:
+        Ys_prime = Ys * ((Sy-1) / (By-1))
+
+    f = np.maximum(0, np.floor(Ys_prime).astype(np.int))
     c = np.minimum(f + 1, Sy - 1)
 
     # w1 = c - Xs_prime
@@ -105,8 +115,13 @@ def bias_jei_zoom(small_bias, factors):
     I = A
     A = np.zeros((Bx, By, Bz))
     Zs = np.arange(Bz)
-    Zs_prime = Zs / factors[2]
-    f = np.floor(Zs_prime).astype(np.int)
+    # Zs_prime = Zs / factors[2]
+    if grid_mode:
+        Zs_prime = (1 - factors[2])/(2 * factors[2]) + Zs/factors[2]
+    else:
+        Zs_prime = Zs * ((Sz-1) / (Bz-1))
+
+    f = np.maximum(0, np.floor(Zs_prime).astype(np.int))
     c = np.minimum(f + 1, Sz - 1)
 
     # w1 = c - Xs_prime
@@ -328,8 +343,8 @@ if __name__ == '__main__':
 
     print(f'Labels Shape is: {LABELS_SHAPE}\n')
 
-    for spacing in [2, 6, 8, 10, 12]:
-        for test in range(5):
+    for spacing in [8]: #, 6, 8, 10, 12]:
+        for test in range(1):
             print(f'Test Run: {test}')
             print(f'Spacing = {spacing}', end=' ')
 
@@ -348,19 +363,19 @@ if __name__ == '__main__':
             if test == 0:
                 print_absolute_diff_table(bias_result_list, header)
 
-            small_def = np.stack([small_bias, small_bias, small_bias], axis=-1)
+            # small_def = np.stack([small_bias, small_bias, small_bias], axis=-1)
 
-            def_field_scipy = def_scipy_zoom(small_def, bias_factors)
-            def_field_jei = def_jei_zoom(small_def, bias_factors)
-            def_field_einsum = einsum_zoom(small_def, bias_factors, 'def')
-            def_field_prod = prod_zoom(small_def, bias_factors, 'def')
+            # def_field_scipy = def_scipy_zoom(small_def, bias_factors)
+            # def_field_jei = def_jei_zoom(small_def, bias_factors)
+            # def_field_einsum = einsum_zoom(small_def, bias_factors, 'def')
+            # def_field_prod = prod_zoom(small_def, bias_factors, 'def')
 
-            def_list = [
-                def_field_scipy, def_field_jei, def_field_einsum,
-                def_field_prod
-            ]
-            if test == 0:
-                print_absolute_diff_table(def_list, header)
+            # def_list = [
+            #     def_field_scipy, def_field_jei, def_field_einsum,
+            #     def_field_prod
+            # ]
+            # if test == 0:
+            #     print_absolute_diff_table(def_list, header)
 
             save_images(bias_result_list, test, header, spacing, 'bias')
-            save_images(def_list, test, header, spacing, 'deformation')
+            # save_images(def_list, test, header, spacing, 'deformation')
